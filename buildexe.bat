13025cef473df39839381f92b5736c30f77ec064
bozhou.fan <bozhou.fan>
2023-09-11 21:47:05 +0800

    增加打包命令

--- buildexe.bat
+++ buildexe.bat
@@ -1,1 +1,5 @@
 pyinstaller --onefile --add-data "performancetest;performancetest" --add-data "performancetest/web/test_result/index.html;performancetest/web/test_result"  --add-data "airtest path;airtest" performancetest/web/main.py -i icon
+
+python setup.py sdist bdist_wheel
+
+python -m twine upload dist/*
